c615b2cf5df24ae3e33a768d3ddc4a600b4c91c6
Love <Love@love.local>
2016-08-20 23:11:17 +0530

    Initial Commit

---
+++ DeviceAdaptibity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeviceAdaptibity
+//
+//  Created by Love on 8/20/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeviceAdaptibity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DeviceAdaptibity
+//
+//  Created by Love on 8/20/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
